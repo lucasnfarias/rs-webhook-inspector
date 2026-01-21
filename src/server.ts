7d6edea081cf2a8aa13aea4cda25e6fc2469f7ec
lucasnfarias <lucasnfarias01@gmail.com>
2026-01-21 13:26:42 -0300

    chore: create initial structture for api and web workspaces

---
+++ src/server.ts
@@ -0,0 +1,50 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+import { listWebhooks } from '@/routes/list-webhooks'
+import { env } from '@/env'
+
+const app = fastify()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  // credentials: true
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Webhook Inspector API',
+      description: 'API for capturing and inpecting webhook requests',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs',
+})
+
+app.register(listWebhooks)
+
+
+app
+  .listen({
+    port: env.PORT,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log(`🚀 HTTP server running on http://localhost:${env.PORT} ...`)
+    console.log(`📖 Docs available on http://localhost:${env.PORT}/docs ...`)
+  })
